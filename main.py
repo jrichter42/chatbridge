fc79a86dead2d0d3ddcafbf426f74edad67f60fc
Jens Richter <mail@jensrichter.info>
2021-01-18 06:01:04 +0100

    Initial discord bot test (working)

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 # chatbridge by jrichter
 
+# https://discordpy.readthedocs.io/en/latest/
+# https://github.com/bbernhard/signal-cli-rest-api
+
+import argparse
+import discord
+
+cacheFolderPath = './chatbridge-cache'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Login successful as user {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Hello'):
+        await message.channel.send('World!')
+
 def main():
-    print('Hello world')
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("--discordtoken", "-dt", help="discord bot token")
+    args = argParser.parse_args()
+
+    discordBotToken = ''
+    if args.discordtoken:
+        discordBotToken = args.discordtoken
+    
+    client.run(discordBotToken)
 
 if __name__ == "__main__":
     main()
